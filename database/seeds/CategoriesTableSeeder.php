276be1bd2d8838a4d497c9a1f1609075d92e6c96
Anton <anto.sobolev@ya.ru>
2018-05-26 15:32:43 +0300

    (fix) categories table seeder

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class CategoryTableSeeder extends Seeder
+class CategoriesTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -12,10 +12,10 @@ class CategoryTableSeeder extends Seeder
     public function run()
     {
         DB::table('categories')->truncate();
-        factory(App\Category::class, rand(1, 10))->create()->each(function ($c1) {
-            factory(App\Category::class, rand(1, 10))->make()->each(function ($c2) use ($c1) {
+        factory(App\Category::class, rand(2, 3))->create()->each(function ($c1) {
+            factory(App\Category::class, rand(1, 3))->make()->each(function ($c2) use ($c1) {
                 $c1->appendNode($c2);
-                factory(App\Category::class, rand(1, 10))->make()->each(function ($c3) use ($c2) {
+                factory(App\Category::class, rand(1, 3))->make()->each(function ($c3) use ($c2) {
                     $c2->appendNode($c3);
                 });
             });
